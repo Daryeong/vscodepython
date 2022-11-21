a93ea3def67da83edb84fd94939a0681f1e71a9c
Choe Daryeong <didi.email.com>
2022-11-21 10:03:18 +0900

    number input 3 commit

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
-# python설치, vs코드 설치, OSS폴더 연동
+# python설치, vs코드 설치, OSS폴더 연동, 커밋, 기본 문법
 # 2022. 11. 21. 
-print("hello")
+print("hello") # hello 출력
 print('hello') # "와 ' 차이 없음
-a = 10
+a = 10 # a값에 10 넣기
 b = 20
-print(a+b)
+print(a+b) #a+b 값 출력
+inputA = int(input("number1 : ")) #input:입력
+inputB = int(input("number2 : "))
+# python 3.x f-string
+print(f"{inputA} + {inputB} = {inputA+inputB}")
